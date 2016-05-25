bc6e89b98cc873f4a23af999665b78bd91c872a8
Edwin Gamboa <Edwin.Gamboa@incowia.com>
2016-05-25 17:32:27 +0200

    [PLAT-509] Check if polymer-name.html exists before adding utility. Generate endpoint for light versions of elements

--- devtools-mappedIn/tasks/createPolymerElementsUtilities.js
+++ devtools-mappedIn/tasks/createPolymerElementsUtilities.js
@@ -7,43 +7,65 @@ module.exports = function (grunt) {
     var polymerElements = grunt.file.readJSON(webpackagePath + '/bower.json').dependencies || [];
     var manifestWebpackagePath = grunt.config.get('manifestWebpackagePath');
     var manifest = grunt.file.readJSON(manifestWebpackagePath);
+    var errorMessages = '';
     for (var key in polymerElements) {
-      console.log('Adding ' + key + ' utility');
-      var elementName = key;
-      var artifactObject = {
-        artifactId: elementName,
-        description: 'Utility to use the ' + elementName + ' polymer element as dependency within a Cubbles component',
-        endpoints: [
-          {
-            endpointId: 'main',
+      var elementPath = webpackagePath + '/' + key + '/' + key + '.html';
+      try {
+        grunt.file.read(elementPath);
+        var artifactObject = {
+          artifactId: key,
+          description: 'Utility to use the ' + key + ' polymer element as dependency within a Cubbles component',
+          endpoints: [
+            {
+              endpointId: 'main',
+              resources: [
+                key + '/' + key + '.html'
+              ],
+              dependencies: [
+                'polymer-1.2.3@1.0.2/polymer/main'
+              ]
+            }
+          ]
+        };
+
+        var lightElementPath = webpackagePath + '/' + key + '/' + key + '-light.html';
+        try {
+          grunt.file.read(lightElementPath);
+          var lightEndpoint = {
+            endpointId: 'light',
             resources: [
-              elementName + '/' + elementName + '.html'
+              key + '/' + key + '-light.html'
             ],
             dependencies: [
               'polymer-1.2.3@1.0.2/polymer/main'
             ]
-          }
-        ]
-      };
+          };
+          artifactObject.endpoints.push(lightEndpoint);
+        } catch (e) {}
 
-      // make sure the artifact-type section does exist
-      if (!manifest.artifacts.utilities) {
-        manifest.artifacts.utilities = [];
-      }
+        // make sure the artifact-type section does exist
+        if (!manifest.artifacts.utilities) {
+          manifest.artifacts.utilities = [];
+        }
 
-      // add or replace the artifact
-      var index = utils.arrayObjectIndexOf(manifest.artifacts.utilities, 'artifactId',
-        artifactObject.artifactId);
-      if (index < 0) {
-        grunt.verbose.writeln(
-          'Pushing the new artifact into manifest.webpackage: ' + artifactObject.artifactId);
-        manifest.artifacts.utilities.push(artifactObject);
-      } else {
-        grunt.verbose.writeln(
-          'Replacing the artifact within manifest.webpackage: ' + artifactObject.artifactId);
-        manifest.artifacts.utilities.splice(index, 1, artifactObject);
+        // add or replace the artifact
+        var index = utils.arrayObjectIndexOf(manifest.artifacts.utilities, 'artifactId',
+          artifactObject.artifactId);
+        if (index < 0) {
+          grunt.verbose.writeln(
+            'Pushing the new artifact into manifest.webpackage: ' + artifactObject.artifactId);
+          manifest.artifacts.utilities.push(artifactObject);
+        } else {
+          grunt.verbose.writeln(
+            'Replacing the artifact within manifest.webpackage: ' + artifactObject.artifactId);
+          manifest.artifacts.utilities.splice(index, 1, artifactObject);
+        }
+        grunt.file.write(manifestWebpackagePath, JSON.stringify(manifest, null, 2));
+        console.log('Added ' + key + ' utility');
+      } catch (e) {
+        errorMessages += e.message + '\n';
       }
-      grunt.file.write(manifestWebpackagePath, JSON.stringify(manifest, null, 2));
     }
+    console.warn(errorMessages);
   });
 };
--- devtools-mappedIn/tasks/installBowerDependencies.js
+++ devtools-mappedIn/tasks/installBowerDependencies.js
@@ -1,14 +1,11 @@
 /*global module*/
+var execSync = require('child_process').execSync;
 'use strict';
 module.exports = function (grunt) {
   grunt.registerTask('installBowerDependencies', 'Install bower dependencies of the active webpackage', function () {
     var webpackagePath = grunt.config.get('param.src');
     console.log('Installing dependencies defined in ' + webpackagePath + '/bower.json ...');
-    var exec = require('child_process').exec;
-    var cb = this.async();
-    exec('bower install', {cwd: webpackagePath}, function (err, stdout, stderr) {
-      console.log(stdout);
-      cb();
-    });
+
+    execSync('bower install', {cwd: webpackagePath});
   });
 };
